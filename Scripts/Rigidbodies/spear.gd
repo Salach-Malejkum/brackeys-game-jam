3e86e99f2b2724012897fd2cb2b80da953af8425
Filip Krężel <45013654+Flixirr@users.noreply.github.com>
2024-09-10 14:28:27 +0200

    add spear prediction

--- Scripts/Rigidbodies/spear.gd
+++ Scripts/Rigidbodies/spear.gd
@@ -6,8 +6,6 @@ extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	collider.disabled = true
-	area_collider.disabled = true
 	apply_force(transform.basis * Vector3(0, 0, -throw_speed * 10))
 
 
@@ -19,14 +17,10 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_tip_body_entered(body: Node3D) -> void:
-	self.freeze = true
+	if body is not Player:
+		self.freeze = true
 	print(body)
 
 
 func _on_tip_body_exited(body: Node3D) -> void:
 	print(body)
-
-
-func _on_compensate_throw_timeout() -> void:
-	collider.disabled = false
-	area_collider.disabled = false
--- Scripts/player_test.gd
+++ Scripts/player_test.gd
@@ -1,5 +1,5 @@
 extends CharacterBody3D
-
+class_name Player
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = 4.5
 @onready var head = $CamRotate/Head
 @onready var camera = $CamRotate/Head/Camera3D
 @onready var r_hand = $CamRotate/Head/Camera3D/RightHand
+@onready var d_hit = $DebugHit
 
 @onready var spear_model = $CamRotate/Head/Camera3D/RightHand/Spear
 
@@ -16,6 +17,9 @@ const BOB_FREQ = 2.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
 
+@export var spear_gravity_scale = 1.0
+@export var spear_mass = 1.0
+@export var spear_prediction_v_multiplier = 15.5
 var spear_packaged = preload("res://Scenes/Rigidbodies/Spear.tscn")
 var thrown_spear_instance : RigidBody3D = null
 
@@ -71,22 +75,61 @@ func _fishing_throw():
 		thrown_spear_instance.queue_free()
 		spear_model.visible = true
 	if Input.is_action_pressed("fishing") and not is_instance_valid(thrown_spear_instance):
-		r_hand.translate(Vector3(0.0, 0.0, 0.1))
+		d_hit.visible = true
+		r_hand.translate(Vector3(0.0, 0.0, 0.025))
 		r_hand.position.z = clamp(r_hand.position.z, 0.0, 1.0)
-		camera.fov += 1
-		camera.fov = clamp(camera.fov, 90, 100)
+		#camera.fov += 1
+		#camera.fov = clamp(camera.fov, 90, 100)
+		_draw_aim(spear_mass, spear_gravity_scale)
 	elif Input.is_action_just_released("fishing"):
 		if not is_instance_valid(thrown_spear_instance):
 			spear_model.visible = false
 			thrown_spear_instance = spear_packaged.instantiate()
+			thrown_spear_instance.mass = spear_mass
+			thrown_spear_instance.gravity_scale = spear_gravity_scale
 			thrown_spear_instance.position = r_hand.global_position
 			thrown_spear_instance.transform.basis = r_hand.global_transform.basis
 			thrown_spear_instance.throw_speed *= r_hand.position.z
 			get_parent().add_child(thrown_spear_instance)
 	else:
+		d_hit.visible = false
 		_smooth_back(0.0)
 
 
 func _smooth_back(target_x : float):
 	r_hand.position.z = lerp(r_hand.position.z, 0.0, 0.5)
-	camera.fov = lerpf(camera.fov, 90, 0.5)
+	#camera.fov = lerpf(camera.fov, 90, 0.5)
+
+
+func _draw_aim(mass, gravity_scale):
+	var d_velocity = -r_hand.global_transform.basis.z as Vector3
+	d_velocity *= spear_prediction_v_multiplier * r_hand.position.z # za chuja nie wiem czy ta wartosc jest idealna, nie wiem jak to wyliczyc, dziala to dziala
+	
+	var t_step := 0.01
+	var start_pos = r_hand.global_position
+	var d_gravity = -ProjectSettings.get_setting("physics/3d/default_gravity", 9.8)
+	d_gravity *= gravity_scale
+	var drag = ProjectSettings.get_setting("physics/3d/default_linear_damp", 0.0)
+	
+	var line_start = start_pos
+	var line_end = start_pos
+	
+	for i in range(1, 200):
+		d_velocity.y += d_gravity * t_step
+		line_end = line_start
+		line_end += d_velocity * t_step
+		
+		d_velocity *= clampf(1.0 - drag * t_step, 0, 1)
+		
+		var ray = _raycast_query(line_start, line_end)
+		if not ray.is_empty():
+			d_hit.global_position = ray["position"]
+		
+		line_start = line_end
+
+func _raycast_query(pointA, pointB) -> Dictionary:
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(pointA, pointB, 1<<0)
+	query.hit_from_inside = false
+	var result = space_state.intersect_ray(query)
+	return result
